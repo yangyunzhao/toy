b073844b740da96b7a6648aeddb0d6426633e592
yangyunzhao <yangyunzhao@qq.com>
2024-01-23 23:23:02 +0800

    增加语法节点配置文件

--- scripts/syntax_gen.py
+++ scripts/syntax_gen.py
@@ -1,8 +1,11 @@
 from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
 import os
 import math
-
-def generate_enum(name, filename):
+import yaml
+# =============================================================================
+# 词法分析器相关逻辑
+def generate_token(name, filename):
     with open(filename, 'r', encoding='utf8') as f:
         kinds = [line.strip().split() for line in f]
         max_length = max(len(kind[0]) for kind in kinds)
@@ -23,5 +26,16 @@ def generate_enum(name, filename):
     with open(output_path, 'w', encoding='utf8') as f:
         f.write(template.render(kinds=kinds, max_length=max_length))
 
-generate_enum('TokenKind', os.path.join(os.path.dirname(__file__), 'TokenKind.txt'))
-generate_enum('TriviaKind', os.path.join(os.path.dirname(__file__), 'TriviaKind.txt'))
+generate_token('TokenKind', os.path.join(os.path.dirname(__file__), 'TokenKind.txt'))
+generate_token('TriviaKind', os.path.join(os.path.dirname(__file__), 'TriviaKind.txt'))
+# =============================================================================
+# 语法分析器相关逻辑
+def load_yaml(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return yaml.safe_load(file)
+
+def generate_syntax():
+    yaml_data = load_yaml(os.path.join(os.path.dirname(__file__), 'syntax.yaml'))
+    print(yaml_data)
+
+generate_syntax()
