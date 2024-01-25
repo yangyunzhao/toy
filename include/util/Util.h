3c7f70f01742bc712a6a8308d11dc4b8cf1c97a8
yangyunzhao <yangyunzhao@qq.com>
2024-01-25 15:25:58 +0800

    增加辅助类 not_null

--- include/util/Util.h
+++ include/util/Util.h
@@ -2,5 +2,16 @@
 
 #include <assert.h>
 #include <string_view>
+#include <concepts>
 
 using namespace std::literals::string_view_literals;
+
+// clang-format off
+template<typename T>
+concept not_null = 
+    !std::is_assignable_v<T&, std::nullptr_t> && 
+    requires(T t) {
+        { *t } -> std::convertible_to<typename std::pointer_traits<T>::element_type&>;
+        { t->*t } -> std::convertible_to<typename std::pointer_traits<T>::element_type*>;
+    };
+// clang-format on
